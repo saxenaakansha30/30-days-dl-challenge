e8ffa1477e5786ea9a943e4f94bed1a239bf5df4
Akansha <saxenaakansha30@gmail.com>
2024-11-21 10:48:03 +0530

    Day 21 code

--- day_20.py
+++ day_20.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
 
 
 # Load and preprocess the dataset
@@ -32,12 +33,16 @@ X_train_normal = X_train[y_train == 0]
 model = Sequential()
 
 # Encoder layer
-model.add(Dense(14, activation='relu', input_shape=(X_train.shape[1], )))
-model.add(Dense(7, activation='relu'))
+model.add(Dense(20, activation='relu', input_shape=(X_train.shape[1], )))
+model.add(Dense(10, activation='relu'))
+
+# Latent representation to capture the kep patterns
+model.add(Dense(5, activation='relu'))
 
 # Decoder layer
-model.add(Dense(14, activation='relu'))
-model.add(Dense(X_train.shape[1], activation='linear'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(X_train.shape[1], activation='linear'))  # Reconstruction of the original input
 
 # Compile the model
 model.compile(
@@ -48,11 +53,35 @@ model.compile(
 # Train the model
 history = model.fit(
     X_train_normal, X_train_normal,
-    epochs=50,
-    batch_size=256,
+    epochs=100,
+    batch_size=128,
     validation_split=0.2,
     verbose=1
 )
+
+# Calculate reconstruction error
+X_val_pred = model.predict(X_val)
+# print(X_val_pred)
+
+# Calculate mean square error for each input sample
+reconstruction_errors = np.mean(np.power((X_val - X_val_pred), 2), axis=1)
+
+print(reconstruction_errors)
+
+threshold = np.percentile(reconstruction_errors[y_val == 0], 95)
+print(f"Threshold for anomaly detection is: {threshold}")
+
+y_prediction = [1 if error > threshold else 0 for error in reconstruction_errors]
+
+# Actual Labels for evaluation
+print(f"Actual Anomalies: {sum(y_val)}, Detected: {sum(y_prediction)}")
+
+# Evaluate the model's performance
+confusion_matrix = confusion_matrix(y_val, y_prediction)
+print(f"Confusion Matrix:\n {confusion_matrix}")
+
+classification_report = classification_report(y_val, y_prediction)
+print(f"Classification Report:\n {classification_report}")
 
 # Visualize the Loss
 plt.figure(figsize=(10, 6))
